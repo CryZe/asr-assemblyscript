60c4e4c6d66683061eb788870329612fae63bb8a
Paweł Zatoka <herganus@gmail.com>
2023-04-19 20:24:41 +0200

    Add StringWatcher

--- watcher.ts
+++ watcher.ts
@@ -66,3 +66,67 @@ export class MemoryWatcher<T> {
         return this.current != this.old;
     }
 }
+
+export class StringWatcher {
+    /**  Current value of the watcher. */
+    current: string = "";
+    /**  The value of the watcher from the previous update cycle. */
+    old: string = "";
+    protected module: string;
+    protected address: Process.Address;
+    protected length: u32;
+    protected useUTF16: bool;
+
+    /**
+     * Creates a watcher with the specified type.
+     * @param moduleName Name of the module to read from.
+     * @param length The length of the string.
+     * @param useUTF16 Decode the string as UTF-16. Defaults to `false`.
+     * @param address Relative base address to use.
+     * @returns {StringWatcher} The instance of a memory watcher with the specified parameters.
+     */
+    constructor(moduleName: string, length: u32, address: Process.Address, useUTF16: bool = false) {
+        this.length = length;
+        this.module = moduleName;
+        this.address = address;
+        this.useUTF16 = useUTF16;
+    }
+
+    /**
+     * Updates the watcher and returns `true` if the value has changed.
+     * @param processId ID of the process to read from.
+     * @returns {bool} Whether the value changed in the current update cycle.
+     */
+    update(processId: Process.ProcessId): bool {
+        this.old = this.current;
+        let changed = false;
+        const bufferSize = this.length * 4;
+        let baseAddress = Process.getModuleAddress(processId, this.module);
+
+        const buf = new ArrayBuffer(bufferSize);
+
+        if (Process.read(processId, (baseAddress + this.address), buf)) {
+            let value = "";
+            if (this.useUTF16) {
+                value = String.UTF16.decode(buf);
+            } else {
+                value = String.UTF8.decode(buf);
+            }
+
+            if (value != this.current) {
+                changed = true;
+            }
+            this.current = value;
+        }
+
+        return changed;
+    }
+
+    /**
+     * Check if the value changed in this update cycle.
+     * @returns {bool} Whether the value changed this update cycle or not.
+     */
+    get changed(): bool {
+        return this.current != this.old;
+    }
+}
